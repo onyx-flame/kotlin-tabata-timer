97fd865935258b846ead424ad5a0da32910d3052
onyx <sovenok997@gmail.com>
2021-11-17 16:37:38 +0300

    Complete CRUD operations for workouts & Layout fix

--- app/src/main/java/com/onyx/tabatatimer/adapter/WorkoutAdapter.kt
+++ app/src/main/java/com/onyx/tabatatimer/adapter/WorkoutAdapter.kt
@@ -2,6 +2,7 @@ package com.onyx.tabatatimer.adapter
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.findNavController
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.onyx.tabatatimer.databinding.WorkoutLayoutAdapterBinding
+import com.onyx.tabatatimer.fragments.HomeFragmentDirections
 import com.onyx.tabatatimer.models.Workout
 
 class WorkoutAdapter: RecyclerView.Adapter<WorkoutAdapter.WorkoutViewHolder>() {
@@ -37,19 +39,31 @@ class WorkoutAdapter: RecyclerView.Adapter<WorkoutAdapter.WorkoutViewHolder>() {
     override fun onBindViewHolder(holder: WorkoutViewHolder, position: Int) {
         val currentWorkout = differ.currentList[position]
         holder.itemBinding.tvWorkoutTitle.text = currentWorkout.title
-        holder.itemBinding.tvWorkoutDetails.text = "228 intervals | 13:37"
+        holder.itemBinding.tvWorkoutDetails.text =
+            "Prepare: ${currentWorkout.prepareTime} sec\n" +
+            "Work: ${currentWorkout.workTime} sec\n" +
+            "Rest: ${currentWorkout.restTime} sec\n" +
+            "Cycles Rest: ${currentWorkout.cyclesRestTime} sec\n" +
+            "Cooldown: ${currentWorkout.coolDownTime} sec\n" +
+            "Cycles: ${currentWorkout.cycles}\n" +
+            "Sets: ${currentWorkout.sets}"
+        holder.itemBinding.tvWorkoutInfo.text = "228 intervals | 13:37"
 
-        val random = java.util.Random()
-        val color = Color.argb(
-            255,
-            random.nextInt(256),
-            random.nextInt(256),
-            random.nextInt(256)
-        )
-        holder.itemBinding.viewColor.setBackgroundColor(color)
+        holder.itemView.setOnClickListener {
+            if (holder.itemBinding.tvWorkoutDetails.visibility == View.GONE) {
+                holder.itemBinding.tvWorkoutDetails.visibility = View.VISIBLE
+            } else {
+                holder.itemBinding.tvWorkoutDetails.visibility = View.GONE
+            }
 
-        holder.itemView.setOnClickListener { view ->
-            Toast.makeText(view.context, "Future Launch!", Toast.LENGTH_LONG).show()
+        }
+        holder.itemBinding.startWorkout.setOnClickListener { view ->
+            Toast.makeText(view.context, "Start Workout!", Toast.LENGTH_SHORT).show()
+        }
+        holder.itemBinding.updateWorkout.setOnClickListener { view ->
+            val direction = HomeFragmentDirections.actionHomeFragmentToUpdateWorkoutFragment(currentWorkout)
+            view.findNavController().navigate(direction)
+            Toast.makeText(view.context, "Edit Workout", Toast.LENGTH_SHORT).show()
         }
     }
 
--- app/src/main/java/com/onyx/tabatatimer/fragments/UpdateWorkoutFragment.kt
+++ app/src/main/java/com/onyx/tabatatimer/fragments/UpdateWorkoutFragment.kt
@@ -1,17 +1,25 @@
 package com.onyx.tabatatimer.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.onyx.tabatatimer.MainActivity
 import com.onyx.tabatatimer.R
 import com.onyx.tabatatimer.databinding.FragmentUpdateWorkoutBinding
+import com.onyx.tabatatimer.models.Workout
+import com.onyx.tabatatimer.viewmodels.WorkoutViewModel
 
 class UpdateWorkoutFragment : Fragment() {
 
     private var _binding: FragmentUpdateWorkoutBinding? = null
     private val binding get() = _binding!!
+    private lateinit var workoutViewModel: WorkoutViewModel
+    private val args: UpdateWorkoutFragmentArgs by navArgs()
+    private lateinit var currentWorkout: Workout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +44,7 @@ class UpdateWorkoutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        workoutViewModel = (activity as MainActivity).workoutViewModel
         binding.apply {
             etPrepareTimeUpdate.transformationMethod = null
             etWorkTimeUpdate.transformationMethod = null
@@ -45,15 +54,84 @@ class UpdateWorkoutFragment : Fragment() {
             etCyclesCountUpdate.transformationMethod = null
             etSetsCountUpdate.transformationMethod = null
         }
+
+        currentWorkout = args.workout!!
+        binding.apply {
+            etTitleUpdate.setText(currentWorkout.title)
+            etColorUpdate.setText(currentWorkout.color)
+            etPrepareTitleUpdate.setText(currentWorkout.prepareDescription)
+            etPrepareTimeUpdate.setText(currentWorkout.prepareTime.toString())
+            etWorkTitleUpdate.setText(currentWorkout.workDescription)
+            etWorkTimeUpdate.setText(currentWorkout.workTime.toString())
+            etRestTitleUpdate.setText(currentWorkout.restDescription)
+            etRestTimeUpdate.setText(currentWorkout.restTime.toString())
+            etCyclesRestTitleUpdate.setText(currentWorkout.cyclesRestDescription)
+            etCyclesRestTimeUpdate.setText(currentWorkout.cyclesRestTime.toString())
+            etCoolDownTitleUpdate.setText(currentWorkout.coolDownDescription)
+            etCoolDownTimeUpdate.setText(currentWorkout.coolDownTime.toString())
+            etCyclesCountUpdate.setText(currentWorkout.cycles.toString())
+            etSetsCountUpdate.setText(currentWorkout.sets.toString())
+        }
+        binding.fabUpdate.setOnClickListener {
+            val workoutTitle = binding.etTitleUpdate.text.toString()
+            val workoutColor = binding.etColorUpdate.text.toString()
+            val workoutPrepareTitle = binding.etPrepareTitleUpdate.text.toString()
+            val workoutPrepareTime = binding.etPrepareTimeUpdate.text.toString().toInt()
+            val workoutWorkTitle = binding.etWorkTitleUpdate.text.toString()
+            val workoutWorkTime = binding.etWorkTimeUpdate.text.toString().toInt()
+            val workoutRestTitle = binding.etRestTitleUpdate.text.toString()
+            val workoutRestTime = binding.etRestTimeUpdate.text.toString().toInt()
+            val workoutCyclesRestTitle = binding.etCyclesRestTitleUpdate.text.toString()
+            val workoutCyclesRestTime = binding.etCyclesRestTimeUpdate.text.toString().toInt()
+            val workoutCoolDownTitle = binding.etCoolDownTitleUpdate.text.toString()
+            val workoutCoolDownTime = binding.etCoolDownTimeUpdate.text.toString().toInt()
+            val workoutCyclesCount = binding.etCyclesCountUpdate.text.toString().toInt()
+            val workoutSetsCount = binding.etSetsCountUpdate    .text.toString().toInt()
+
+            val workout = Workout(
+                currentWorkout.id,
+                workoutTitle,
+                workoutColor,
+                workoutPrepareTitle,
+                workoutPrepareTime,
+                workoutWorkTitle,
+                workoutWorkTime,
+                workoutRestTitle,
+                workoutRestTime,
+                workoutCyclesRestTitle,
+                workoutCyclesRestTime,
+                workoutCoolDownTitle,
+                workoutCoolDownTime,
+                workoutCyclesCount,
+                workoutSetsCount
+            )
+
+            workoutViewModel.updateWorkout(workout)
+            Snackbar.make(view, "Workout Updated!", Snackbar.LENGTH_SHORT).show()
+            view.findNavController().navigate(R.id.action_updateWorkoutFragment_to_homeFragment)
+        }
+    }
+
+    private fun deleteWorkout() {
+        AlertDialog.Builder(activity).apply {
+            setTitle("Delete Workout")
+            setMessage("Are you sure to delete this workout?")
+            setPositiveButton("Delete") { _,_ ->
+                workoutViewModel.deleteWorkout(currentWorkout)
+                Snackbar.make(requireView(), "Workout deleted!", Snackbar.LENGTH_SHORT).show()
+                view?.findNavController()?.navigate(R.id.action_updateWorkoutFragment_to_homeFragment)
+            }
+            setNegativeButton("Cancel", null)
+        }.create().show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.delete_menu -> {
-                Unit
+                deleteWorkout()
             }
             android.R.id.home -> {
-                view?.findNavController()?.navigate(R.id.action_newWorkoutFragment_to_homeFragment)
+                view?.findNavController()?.navigate(R.id.action_updateWorkoutFragment_to_homeFragment)
             }
         }
         return super.onOptionsItemSelected(item)
